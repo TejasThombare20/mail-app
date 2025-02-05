1ee92c4ddbbabef17e8fb09cc180e765ce14e51b
TejasThombare20 <img_2020066@iiitm.ac.in>
2025-02-06 00:02:26 +0530

    feat : base structure for client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main className='text-red-500 text-5xl'>
+      hello
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/reportWebVitals.ts
@@ -0,0 +1,20 @@
+type ReportHandler = (metric: {
+  name: string;
+  value: number;
+  delta: number;
+  id: string;
+}) => void;
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
